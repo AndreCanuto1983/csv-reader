e8169003b337589587ebd916f8bf1681b64b412b
André Canuto <andrecanuto@outlook.com.br>
2024-02-09 13:04:36 -0300

    Feat; 2 maneiras para ler csv

--- CsvReader/Program.cs
+++ CsvReader/Program.cs
@@ -1,13 +1,54 @@
-﻿//csv file path
-var fileData = File.ReadLines("C:\\Users\\andre\\Downloads\\K3241.K03200Y1.D40113.ESTABELE.csv");
+﻿static List<string> CsvFileReaderV1(string path)
+{
+    var listData = new List<string>();
 
-var data = new List<string>();
+    using (StreamReader stream = new StreamReader(path))
+    {
+        string data = stream.ReadLine();
+        string[] arrData = data.Split(';');
+
+        do
+        {
+            for (int i = 0; i <= arrData.GetUpperBound(0); i++)
+            {
+                listData.Add(arrData[i]);
+            }
+
+        } while (!(stream.EndOfStream));
+    }
+
+    return listData;
+}
+
+static List<string> CsvFileReaderV2(string path)
+{
+    var data = File.ReadLines(path);
+    var listData = new List<string>();
+
+    foreach (var line in data)
+    {
+        listData.Add(line);
+    }
+
+    return listData;
+}
+
+var dataV1 = CsvFileReaderV1("C:\\Users\\andre\\Downloads\\K3241.K03200Y1.D40113.ESTABELE.csv");
+
+var dataV2 = CsvFileReaderV2("C:\\Users\\andre\\Downloads\\K3241.K03200Y1.D40113.ESTABELE.csv");
+
 int count = 0;
 
-foreach (var line in fileData)
+foreach (var line in dataV1)
 {
     count += 1;
     Console.WriteLine();
     Console.WriteLine(count + " - " + line);
-    data.Add(line);
+}
+
+foreach (var line in dataV2)
+{
+    count += 1;
+    Console.WriteLine();
+    Console.WriteLine(count + " - " + line);
 }

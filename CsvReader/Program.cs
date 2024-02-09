a047709c75399e7cf86afc4574231ab1cdccbbc5
André Canuto <andrecanuto@outlook.com.br>
2024-02-08 21:38:26 -0300

    Feat: example of how to read a csv file

---
+++ CsvReader/Program.cs
@@ -0,0 +1,13 @@
+﻿//csv file path
+var fileData = File.ReadLines("C:\\Users\\andre\\Downloads\\K3241.K03200Y1.D40113.ESTABELE.csv");
+
+var data = new List<string>();
+int count = 0;
+
+foreach (var line in fileData)
+{
+    count += 1;
+    Console.WriteLine();
+    Console.WriteLine(count + " - " + line);
+    data.Add(line);
+}
